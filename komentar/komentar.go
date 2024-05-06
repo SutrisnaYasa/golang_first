c1c4e1de442bf1193d3abe0ffc735b08f6eb1598
SutrisnaYasa <sutrisnayasa6@gmail.com>
2024-05-06 11:04:35 +0800

    komentar

---
+++ komentar/komentar.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// komentar kode
+	// menampilkan pesan hello world
+	fmt.Println("hello world")
+
+	// fmt.Println("baris ini tidak akan dieksekusi")
+
+	/*
+	   komentar kode
+	   menampilkan pesan hello world
+	*/
+	fmt.Println("Komentar multi line")
+}
