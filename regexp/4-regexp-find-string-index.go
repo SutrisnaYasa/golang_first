d45f8a7d19bccfab8be8d92c37b6f2bc8ae72738
SutrisnaYasa <sutrisnayasa6@gmail.com>
2024-05-20 15:20:52 +0800

    regexp

---
+++ regexp/4-regexp-find-string-index.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	var text = "banana burger soup"
+	var regex, _ = regexp.Compile(`[a-z]+`)
+
+	var idx = regex.FindStringIndex(text)
+	fmt.Println(idx)
+	// [0, 6]
+
+	var str = text[0:6]
+	fmt.Println(str)
+	// "banana"
+}
